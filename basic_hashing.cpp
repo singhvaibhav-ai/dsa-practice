ecb77b28ec212c7beb2d14b43f1f34bb38e8784d
singhvaibhav-ai <vaibhavsingh220406@gmail.com>
2025-08-05 00:30:02 +0530

    optimal soln added

--- basic_hashing.cpp
+++ basic_hashing.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 // Highest occurring element in an array
 // Brute force approach
+// TC = O(n^2)
+// SC = O(1) → technically O(10^4) but considered constant
 int highest_occurrence(int arr[], int n)
 {
     int vis[10001] = {0};
@@ -29,6 +31,37 @@ int highest_occurrence(int arr[], int n)
             {
                 el = arr[i];
             }
+        }
+    }
+    return el;
+}
+
+// Optimal Approach
+// TC = O(n)
+// SC = O(max(arr)) → better than fixed-size 10^4
+int highest_occurrence_optimal(int arr[], int n)
+{
+    int maxNum = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        maxNum = max(maxNum, arr[i]);
+    }
+    int hash[maxNum + 1] = {0};
+    for (int i = 0; i < n; i++)
+    {
+        hash[arr[i]]++;
+    }
+    int maxCount = 0, el = -1;
+    for (int i = 0; i <= maxNum; i++)
+    {
+        if (hash[i] > maxCount)
+        {
+            maxCount = hash[i];
+            el = i;
+        }
+        else if (hash[i] == maxCount && i < el)
+        {
+            el = i;
         }
     }
     return el;
