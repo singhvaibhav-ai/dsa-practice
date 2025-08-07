0cbba73d327c39a0fa58db1d06526f9abfdb4043
singhvaibhav-ai <vaibhavsingh220406@gmail.com>
2025-08-07 12:57:50 +0530

    2 qns added

--- basic_strings.cpp
+++ basic_strings.cpp
@@ -71,6 +71,26 @@ string largestOddNumber(string &s)
     return s.substr(i, j - i + 1);
 }
 
+// Longest common prefix
+string longestCommonPrefix(vector<string> &strs)
+{
+    if (strs.empty())
+    {
+        return "";
+    }
+    sort(strs.begin(), strs.end());
+    string first = strs[0];
+    string last = strs[strs.size() - 1];
+    string ans = "";
+    for (int i = 0; i < min(first.size(), last.size()); i++)
+    {
+        if (first[i] != last[i])
+            break;
+        ans += first[i];
+    }
+    return ans;
+}
+
 int main()
 {
     return 0;
