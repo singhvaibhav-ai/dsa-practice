8e799f7b06d80d6bd9aaa81e6b0aefc3a92750f1
singhvaibhav-ai <vaibhavsingh220406@gmail.com>
2025-08-10 19:16:13 +0530

    nNumbersSum added

--- basic_recursion.cpp
+++ basic_recursion.cpp
@@ -1,7 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int nNumbersSum(int n)
+{
+    if (n == 0)
+        return 0;
+    return n + nNumbersSum(n - 1);
+}
+
 int main()
 {
+    int n;
+    cin >> n;
+    cout << nNumbersSum(n);
     return 0;
 }
