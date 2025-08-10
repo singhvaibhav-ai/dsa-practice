3c9b3929cb15be7f4f8ba3f4aae09a649887a637
singhvaibhav-ai <vaibhavsingh220406@gmail.com>
2025-08-10 20:17:43 +0530

    factorial added

--- basic_recursion.cpp
+++ basic_recursion.cpp
@@ -1,11 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sum of First n Numbers
+//  TC = O(n)
+//  SC = O(n) due to recursion stack
 int nNumbersSum(int n)
 {
     if (n == 0)
         return 0;
     return n + nNumbersSum(n - 1);
+}
+
+// Factorial of a Given Number
+//  TC = O(n)
+//  SC = O(n) due to recursion stack
+long long int factorial(int i, int n)
+{
+    if (i > n)
+        return 1;
+    return i * factorial(i + 1, n);
 }
 
 int main()
