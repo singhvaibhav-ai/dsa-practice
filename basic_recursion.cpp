21587b12a56c1f2abe3484697b7fef97403f374c
singhvaibhav-ai <vaibhavsingh220406@gmail.com>
2025-08-11 17:50:38 +0530

    array reversal

--- basic_recursion.cpp
+++ basic_recursion.cpp
@@ -90,6 +90,23 @@ bool checkPrime(int num)
     return checkPrimeHelper(num, 2);
 }
 
+// Reverse an Array
+// TC = O(n)
+// SC = O(n) due to recursion stack
+void reverse(vector<int> &nums, int left, int right)
+{
+    if (left >= right)
+        return;
+    swap(nums[left], nums[right]);
+    reverse(nums, left + 1, right - 1);
+}
+
+vector<int> reverseArray(vector<int> &nums)
+{
+    reverse(nums, 0, nums.size() - 1);
+    return nums;
+}
+
 int main()
 {
     int n;
