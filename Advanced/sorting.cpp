db87b33bba5ae384cb02ca00ea934102d9345ffd
singhvaibhav-ai <vaibhavsingh220406@gmail.com>
2025-08-13 01:23:26 +0530

    selection sort

--- Advanced/sorting.cpp
+++ Advanced/sorting.cpp
@@ -1,6 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Selection Sort
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
 int main()
 {
     return 0;
