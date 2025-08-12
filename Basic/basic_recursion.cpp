2c6265be7ad887f03a49bf90c21575386a41067f
singhvaibhav-ai <vaibhavsingh220406@gmail.com>
2025-08-12 17:14:35 +0530

    recusrion fibonacci edited

--- Basic/basic_recursion.cpp
+++ Basic/basic_recursion.cpp
@@ -145,10 +145,8 @@ int sumOfDigits(int n)
 // SC = O(n) due to recursion stack
 int fib(int n)
 {
-    if (n == 0)
-        return 0;
-    if (n == 1)
-        return 1;
+    if (n <= 1)
+        return n;
     return fib(n - 1) + fib(n - 2);
 }
 
