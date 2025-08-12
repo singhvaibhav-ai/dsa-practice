2a80019f775e63ac19b4b43ad72c8ab307e7fd71
singhvaibhav-ai <vaibhavsingh220406@gmail.com>
2025-08-12 16:52:41 +0530

    FIBONACCI NUMBERS

--- Basic/basic_recursion.cpp
+++ Basic/basic_recursion.cpp
@@ -140,6 +140,18 @@ int sumOfDigits(int n)
     return sumOfDigits(sum(n));
 }
 
+// Fibonacci Number
+// TC = O(2^n)
+// SC = O(n) due to recursion stack
+int fib(int n)
+{
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    return fib(n - 1) + fib(n - 2);
+}
+
 int main()
 {
     int n;
