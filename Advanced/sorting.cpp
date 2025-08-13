6b47fafd3227eb856437d0ff2e85a8df70504a85
singhvaibhav-ai <vaibhavsingh220406@gmail.com>
2025-08-13 11:16:45 +0530

    bubble sort optimised

--- Advanced/sorting.cpp
+++ Advanced/sorting.cpp
@@ -30,13 +30,17 @@ void bubbleSort(int arr[], int n)
 {
     for (int i = 0; i < n - 1; i++)
     {
+        bool didSwap = false;
         for (int j = 0; j < n - i - 1; j++)
         {
             if (arr[j] > arr[j + 1])
             {
                 swap(arr[j], arr[j + 1]);
+                didSwap = true;
             }
         }
+        if (didSwap == false)
+            break;
     }
 }
 
