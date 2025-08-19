7b38150a17ca3eb43f34efbe519bc4a7ab35b910
singhvaibhav-ai <vaibhavsingh220406@gmail.com>
2025-08-19 13:42:40 +0530

    linear search

---
+++ Advanced/Arrays/Fundamentals/linear_search.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int linearSearch(vector<int> &nums, int target)
+{
+    int n = nums.size();
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] == target)
+            return i;
+    }
+    return -1;
+}
+int main()
+{
+    vector<int> nums = {1, 2, 3, 4, 5};
+    int target = 4;
+    cout << linearSearch(nums, target) << endl;
+    return 0;
+}
