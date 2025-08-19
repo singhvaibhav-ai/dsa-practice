c62529b484016583d758115f82eab2a90121d6fd
singhvaibhav-ai <vaibhavsingh220406@gmail.com>
2025-08-19 13:47:24 +0530

    largest element

---
+++ Advanced/Arrays/Fundamentals/largest_element.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int largestElement(vector<int> &nums)
+{
+    int n = nums.size();
+    int max = nums[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (max < nums[i])
+            max = nums[i];
+    }
+    return max;
+}
+int main()
+{
+    vector<int> nums = {3, 2, 1, 5, 2};
+    cout << largestElement(nums) << endl;
+    return 0;
+}
