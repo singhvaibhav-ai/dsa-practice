eeca8aef824c82d7455df05d5303ef4744b91b86
singhvaibhav-ai <vaibhavsingh220406@gmail.com>
2025-09-11 06:44:21 +0530

    largest element

---
+++ Advanced/Arrays/Fundamentals/2_largest_element.py
@@ -0,0 +1,13 @@
+def largest_element(nums):
+    if not nums:
+        return None  # Handle empty array case
+    largest = nums[0]
+    for num in nums:
+        if num > largest:
+            largest = num
+    return largest
+
+
+nums = [3, 1, 4, 1, 5, 9, 2, 6, 5]
+result = largest_element(nums)
+print(result)
