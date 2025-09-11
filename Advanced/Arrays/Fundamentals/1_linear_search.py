80131e826c655f6bb089da859e1754248c0cc437
singhvaibhav-ai <vaibhavsingh220406@gmail.com>
2025-09-11 06:41:35 +0530

    cpp -> python first commit

---
+++ Advanced/Arrays/Fundamentals/1_linear_search.py
@@ -0,0 +1,11 @@
+def linearSearch(nums, target):
+    for i in range(len(nums)):
+        if nums[i] == target:
+            return i
+    return -1
+
+
+nums = [1, 2, 3, 4, 5]
+target = 4
+result = linearSearch(nums, target)
+print(result)
