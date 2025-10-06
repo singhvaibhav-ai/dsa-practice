c8954b9c526b801ab22cd3de857d3af8feba9dd3
singhvaibhav-ai <vaibhavsingh220406@gmail.com>
2025-10-06 18:19:04 +0530

    edited

--- Advanced/Arrays/Logic Building/4_union_of_2_sorted_array.py
+++ Advanced/Arrays/Logic Building/4_union_of_2_sorted_array.py
@@ -24,33 +24,39 @@ def unionArrayBrute2(nums1, nums2):
 # Optimal Approach 1 (Classic Two Pointer Technique)
 # TC = O(n+m)
 # SC = O(n+m)
-def unionArrayOptimal1(nums1, nums2):
+# Optimal Approach (Refined)
+# TC = O(n+m)
+# SC = O(n+m)
+def unionArrayOptimalRefined(nums1, nums2):
     n = len(nums1)
     m = len(nums2)
     i, j = 0, 0
     union = []
+
     while i < n and j < m:
-        if nums1[i] < nums2[j]:
-            if len(union) == 0 or union[-1] != nums1[i]:
+        if nums1[i] <= nums2[j]:
+
+            if not union or union[-1] != nums1[i]:
                 union.append(nums1[i])
+
+            if nums1[i] == nums2[j]:
+                j += 1
             i += 1
-        elif nums1[i] > nums2[j]:
-            if len(union) == 0 or union[-1] != nums2[j]:
+        else:  # nums1[i] > nums2[j]
+            if not union or union[-1] != nums2[j]:
                 union.append(nums2[j])
             j += 1
-        else:
-            if len(union) == 0 or union[-1] != nums1[i]:
-                union.append(nums1[i])
-            i += 1
-            j += 1
+
     while i < n:
-        if len(union) == 0 or union[-1] != nums1[i]:
+        if not union or union[-1] != nums1[i]:
             union.append(nums1[i])
         i += 1
+
     while j < m:
-        if len(union) == 0 or union[-1] != nums2[j]:
+        if not union or union[-1] != nums2[j]:
             union.append(nums2[j])
         j += 1
+
     return union
 
 
