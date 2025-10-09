5eb62fdc46cfb453364b409b21f0e654f51ec342
singhvaibhav-ai <vaibhavsingh220406@gmail.com>
2025-10-09 13:55:12 +0530

    optimal soln added

--- Advanced/Arrays/Logic Building/5_intersection_of_2_sorted_arrays.cpp
+++ Advanced/Arrays/Logic Building/5_intersection_of_2_sorted_arrays.cpp
@@ -1,12 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// intersection of 2 sorted arrays
-vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
+// Intersection of two sorted arrays
+// Brute Force Approach
+// TC = O(n*m)
+// SC = O(m)
+vector<int> intersectionArrayBrute(vector<int> &nums1, vector<int> &nums2)
+{
+    vector<int> ans;
+    vector<int> visited(nums2.size(), 0);
+    int n = nums1.size();
+    int m = nums2.size();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (nums1[i] == nums2[j] && visited[j] == 0)
+            {
+                ans.push_back(nums2[j]);
+                visited[j] = 1;
+                break;
+            }
+            else if (nums2[j] > nums1[i])
+                break;
+        }
+    }
+    return ans;
+}
+
+// Optimal Approach
+// TC = O(n + m)
+// SC = O(1)
+vector<int> intersectionArrayOptimal(vector<int> &nums1, vector<int> &nums2)
 {
     int n = nums1.size(), m = nums2.size();
     int i = 0, j = 0;
-    vector<int> inter;
+    vector<int> intersection;
 
     while (i < n && j < m)
     {
@@ -20,12 +49,11 @@ vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
         }
         else
         {
-            if (inter.empty() || inter.back() != nums1[i])
-                inter.push_back(nums1[i]);
+            intersection.push_back(nums1[i]);
             i++;
             j++;
         }
     }
 
-    return inter;
+    return intersection;
 }
