4e5bd61b1e70ef00bf518f869070e43b6e0924f4
singhvaibhav-ai <vaibhavsingh220406@gmail.com>
2026-04-03 13:35:45 +0530

    I AM BACK!!!

---
+++ Advanced/Binary Search/Fundamentals/Search_X_in_sorted_array.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int search(vector<int> &nums, int target)
+{
+    int n = nums.size();
+    int low = 0, high = n - 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] == target)
+        {
+            return mid;
+        }
+        else if (nums[mid] > target)
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int target = 5;
+    int result = search(nums, target);
+    if (result != -1)
+    {
+        cout << "Element found at index: " << result << endl;
+    }
+    else
+    {
+        cout << "Element not found in the array." << endl;
+    }
+    return 0;
+}
