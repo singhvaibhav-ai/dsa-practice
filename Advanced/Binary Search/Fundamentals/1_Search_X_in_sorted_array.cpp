5bd5326333540d99e4ab27513ba0091f1b4c01db
singhvaibhav-ai <vaibhavsingh220406@gmail.com>
2026-04-03 15:13:41 +0530

    lower bound bhi ho gya guyzz

--- Advanced/Binary Search/Fundamentals/1_Search_X_in_sorted_array.cpp
+++ Advanced/Binary Search/Fundamentals/1_Search_X_in_sorted_array.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int search(vector<int> &nums, int target)
+// TC = O(log2 n)
+// SC = O(1) for iterative, O(log n) for recursive due to call
+// Iterative approach
+int binary_search_iterative(vector<int> &nums, int target)
 {
     int n = nums.size();
     int low = 0, high = n - 1;
@@ -24,11 +26,33 @@ int search(vector<int> &nums, int target)
     return -1;
 }
 
+// Recursive approach
+int binary_search_recursive(vector<int> &nums, int target, int low, int high)
+{
+    if (low > high)
+    {
+        return -1;
+    }
+    int mid = low + (high - low) / 2;
+    if (nums[mid] == target)
+    {
+        return mid;
+    }
+    else if (nums[mid] > target)
+    {
+        return binary_search_recursive(nums, target, low, mid - 1);
+    }
+    else
+    {
+        return binary_search_recursive(nums, target, mid + 1, high);
+    }
+}
+
 int main()
 {
     vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     int target = 5;
-    int result = search(nums, target);
+    int result = binary_search_recursive(nums, target, 0, nums.size() - 1);
     if (result != -1)
     {
         cout << "Element found at index: " << result << endl;
