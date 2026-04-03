f84f31f7c24c61fef23e9c038dca69167ac59f44
singhvaibhav-ai <vaibhavsingh220406@gmail.com>
2026-04-03 15:21:20 +0530

    brute lower bound

--- Advanced/Binary Search/Fundamentals/2_Lower_Bound.cpp
+++ Advanced/Binary Search/Fundamentals/2_Lower_Bound.cpp
@@ -1,5 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+// Brute force approach
+// TC = O(n)
+// SC = O(1)
+int lower_bound_brute(vector<int> &nums, int x)
+{
+    int n = nums.size();
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] >= x)
+        {
+            return i;
+        }
+    }
+    return n;
+}
+
 // Optimal approach
 // TC = O(log2 n)
 // SC = O(1)
