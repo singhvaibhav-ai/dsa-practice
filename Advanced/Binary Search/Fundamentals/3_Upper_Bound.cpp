592cec83c3886ebca643593a76006468d87824d4
singhvaibhav-ai <vaibhavsingh220406@gmail.com>
2026-04-05 14:09:51 +0530

    upper bound optimal

--- Advanced/Binary Search/Fundamentals/3_Upper_Bound.cpp
+++ Advanced/Binary Search/Fundamentals/3_Upper_Bound.cpp
@@ -16,3 +16,38 @@ int upper_bound_brute(vector<int> &nums, int x)
     }
     return n;
 }
+
+// Optimal Approach
+int upper_bound_optimal(vector<int> &nums, int x)
+{
+    int n = nums.size();
+    int low = 0, high = n - 1;
+    int ans = n;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] > x)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+            low = mid + 1;
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int x = 5;
+    int result = upper_bound_optimal(nums, x);
+    if (result != nums.size())
+    {
+        cout << "Upper bound of " << x << " is at index: " << result << endl;
+    }
+    else
+    {
+        cout << "Upper bound of " << x << " does not exist in the array." << endl;
+    }
+}
