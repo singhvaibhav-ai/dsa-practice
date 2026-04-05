ba601c16f169f482882706dd8a077936d1a76158
singhvaibhav-ai <vaibhavsingh220406@gmail.com>
2026-04-05 15:32:49 +0530

    first and last occurence optimal using binary search

--- Advanced/Binary Search/Logic Building/3_First_and_last_occurrence.cpp
+++ Advanced/Binary Search/Logic Building/3_First_and_last_occurrence.cpp
@@ -4,7 +4,7 @@ using namespace std;
 // Brute force approach
 // TC = O(n)
 // SC = O(1)
-vector<int> searchRange(vector<int> &nums, int target)
+vector<int> searchRangeBrute(vector<int> &nums, int target)
 {
     int n = nums.size();
     int first = -1, last = -1;
@@ -38,3 +38,68 @@ vector<int> searchRangeOptimal(vector<int> &nums, int target)
 // Optimal approach using binary search
 // TC = O(log2 n)
 // SC = O(1)
+int firstOccurrence(vector<int> &nums, int target)
+{
+    int low = 0, high = nums.size() - 1;
+    int first = -1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] == target)
+        {
+            first = mid;
+            high = mid - 1;
+        }
+        else if (nums[mid] < target)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return first;
+}
+
+int lastOccurrence(vector<int> &nums, int target)
+{
+    int low = 0, high = nums.size() - 1;
+    int last = -1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] == target)
+        {
+            last = mid;
+            low = mid + 1;
+        }
+        else if (nums[mid] < target)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return last;
+}
+
+vector<int> searchRange(vector<int> &nums, int target)
+{
+    int first = firstOccurrence(nums, target);
+    if (first == -1)
+        return {-1, -1};
+    int last = lastOccurrence(nums, target);
+    return {first, last};
+}
+
+int main()
+{
+    vector<int> nums = {5, 7, 7, 8, 8, 10};
+    int target = 8;
+    vector<int> result = searchRange(nums, target);
+    cout << "First and Last Occurrence of " << target << " is at indices: [" << result[0] << ", " << result[1] << "]" << endl;
+    return 0;
+}
