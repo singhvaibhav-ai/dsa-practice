1ab0a247da335a448cce61583d9e481540dd5db7
singhvaibhav-ai <vaibhavsingh220406@gmail.com>
2026-04-06 14:14:53 +0530

    optimal added

--- Advanced/Binary Search/Logic Building/4_Search_in_rotated_sorted_array-I.cpp
+++ Advanced/Binary Search/Logic Building/4_Search_in_rotated_sorted_array-I.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Linear Search Approach
+// Brute Force Approach using Linear Search
 // TC = O(n)
 // SC = O(1)
 int searchLinear(vector<int> &nums, int target)
@@ -11,6 +11,43 @@ int searchLinear(vector<int> &nums, int target)
     {
         if (nums[i] == target)
             return i;
+    }
+    return -1;
+}
+
+// Optimal Approach using Binary Search
+// TC = O(log2 n)
+// SC = O(1)
+int searchBinary(vector<int> &nums, int target)
+{
+    int low = 0, high = nums.size() - 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] == target)
+            return mid;
+        if (nums[low] <= nums[mid])
+        {
+            if (nums[low] <= target && target <= nums[mid])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        else
+        {
+            if (nums[mid] <= target && target <= nums[high])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
     }
     return -1;
 }
