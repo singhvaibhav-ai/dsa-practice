aaa9954fde910db1ca90619e2cd4988d16bfabf6
singhvaibhav-ai <vaibhavsingh220406@gmail.com>
2026-04-14 16:46:09 +0530

    no of rotation optimal done

--- Advanced/Binary Search/Logic Building/7_Find_out_how_many_times_the_array_is_rotated.cpp
+++ Advanced/Binary Search/Logic Building/7_Find_out_how_many_times_the_array_is_rotated.cpp
@@ -18,3 +18,32 @@ int findKRotationBrute(vector<int> &nums)
     }
     return index;
 }
+
+// Optimal Approach using Binary Search
+// TC = O(log2 n)
+// SC = O(1)
+int findKRotationOptimal(vector<int> &nums)
+{
+    int n = nums.size();
+    int low = 0, high = n - 1;
+    while (low < high)
+    {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] > nums[high])
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid;
+        }
+    }
+    return low;
+}
+
+int main()
+{
+    vector<int> nums = {15, 18, 2, 3, 6, 12};
+    cout << "The array is rotated " << findKRotationOptimal(nums) << " times." << endl;
+    return 0;
+}
