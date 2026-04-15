5b9059af0a22b2aa94f39fea9dca5390e54342d4
singhvaibhav-ai <vaibhavsingh220406@gmail.com>
2026-04-15 15:15:58 +0530

    bichara single element using ls

--- Advanced/Binary Search/Logic Building/8_Single_element_in_sorted_array.cpp
+++ Advanced/Binary Search/Logic Building/8_Single_element_in_sorted_array.cpp
@@ -14,3 +14,38 @@ int singleNonDuplicateXOR(vector<int> &nums)
     }
     return ans;
 }
+
+// Brute Force Approach using Linear Search
+// TC = O(n)
+// SC = O(1)
+int singleNonDuplicateLS(vector<int> &nums)
+{
+    int n = nums.size();
+    if (n == 1)
+        return nums[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (i == 0)
+        {
+            if (nums[i] != nums[i + 1])
+            {
+                return nums[i];
+            }
+        }
+        else if (i == n - 1)
+        {
+            if (nums[i] != nums[i - 1])
+            {
+                return nums[i];
+            }
+        }
+        else
+        {
+            if (nums[i] != nums[i - 1] && nums[i] != nums[i + 1])
+            {
+                return nums[i];
+            }
+        }
+    }
+    return -1;
+}
