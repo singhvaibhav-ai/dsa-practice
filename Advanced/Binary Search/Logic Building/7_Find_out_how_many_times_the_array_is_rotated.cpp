fb6e1cdc1f30e19d8bd8d5a88783cce1b4ea976c
singhvaibhav-ai <vaibhavsingh220406@gmail.com>
2026-04-17 15:23:48 +0530

    Strivers good practice added

--- Advanced/Binary Search/Logic Building/7_Find_out_how_many_times_the_array_is_rotated.cpp
+++ Advanced/Binary Search/Logic Building/7_Find_out_how_many_times_the_array_is_rotated.cpp
@@ -41,6 +41,46 @@ int findKRotationOptimal(vector<int> &nums)
     return low;
 }
 
+// Striver
+int findKRotation(vector<int> &nums)
+{
+    int low = 0, high = nums.size() - 1;
+    int ans = INT_MAX;
+    int index = -1;
+    while (low <= high) // used low <= high for consistency in coding
+    {
+        int mid = (low + high) / 2;
+        if (nums[low] <= nums[high])
+        {
+            if (nums[low] < ans)
+            {
+                index = low;
+                ans = nums[low];
+            }
+            break;
+        }
+        if (nums[low] <= nums[mid])
+        {
+            if (nums[low] < ans)
+            {
+                index = low;
+                ans = nums[low];
+            }
+            low = mid + 1;
+        }
+        else
+        {
+            if (nums[mid] < ans)
+            {
+                index = mid;
+                ans = nums[mid];
+            }
+            high = mid - 1;
+        }
+    }
+    return index;
+}
+
 int main()
 {
     vector<int> nums = {15, 18, 2, 3, 6, 12};
